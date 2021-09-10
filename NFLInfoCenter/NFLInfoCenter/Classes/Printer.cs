b6ba4eab41bd2e1ae5876657d07ab5b8c647d86a
Julio Leija <j.leija@babinc.com>
2021-09-10 16:32:38 -0600

    Documenting Printer class.

--- NFLInfoCenter/NFLInfoCenter/Classes/Printer.cs
+++ NFLInfoCenter/NFLInfoCenter/Classes/Printer.cs
@@ -6,27 +6,19 @@ using System.Threading.Tasks;
 using ZXing;
 using System.Net;
 
-
-
 namespace NFLInfoCenter.Classes
 {
     class Printer
     {
-        
         public string printerName { get; set; }
-
         private System.Drawing.Printing.PrintDocument printer;
         private System.Windows.Forms.Form commingFrom;
-        
         private string burntPrinterName = "zebralocalprinter";
-
         private string hostname;
         private string stationName;
 
         private Receipt receipt;
-
         private PreReceipts prereceipt;
-
         public Printer(System.Windows.Forms.Form commingFrom, System.Drawing.Printing.PrintDocument printer)
         {
             this.printerName = burntPrinterName;
@@ -45,7 +37,11 @@ namespace NFLInfoCenter.Classes
             
 
         }
-        
+
+        /// <summary>
+        /// Gets the serial number value set for the global Receipt object inside this class.
+        /// </summary>
+        /// <returns>The serial number value of the global Receipt of the class.</returns>
         public string get_sn() {
             try
             {
@@ -62,20 +58,41 @@ namespace NFLInfoCenter.Classes
                 return "";
             }
         }
+        /// <summary>
+        /// Sets/modifies the printer properti of PrintingDocument in this class.
+        /// </summary>
+        /// <param name="name"></param>
         public void setPrinterName(string name)
         {
             this.printerName = name;
             this.printer.PrinterSettings.PrinterName = this.printerName;
         }
+        /// <summary>
+        /// Sets the contents of the global Receipt object of the class.
+        /// </summary>
+        /// <param name="receipt"></param>
         public void set_receipt(Receipt receipt)
         {
             this.receipt = receipt;
         }
+        /// <summary>
+        /// Sets the contents of the global PreReceipt object of the class.
+        /// </summary>
+        /// <param name="prereceipt"></param>
         public void set_prereceipt(PreReceipts prereceipt)
         {
             this.prereceipt = prereceipt;
         }
-  
+        /// <summary>
+        /// Sets the DocumentName property of the PrintingDocument and calls its Print event.
+        /// <br/><br/>
+        /// Remarks:
+        /// <br/>
+        /// <list type="number">
+        /// <item>Data to print is passed through the global Receipt objects.</item>
+        /// </list>
+        /// </summary>
+        /// <param name="receipt"></param>
         public void printUnitLabel(Receipt receipt)
         {
             Console.WriteLine("10 printUnitLabel: sku " + receipt.Sku + " qty " +  receipt.Qty);
@@ -94,7 +111,17 @@ namespace NFLInfoCenter.Classes
                 Console.WriteLine("11.2 receipt was null.");
             }
         }
-
+        /// <summary>
+        ///  Sets the DocumentName property of the PrintingDocument and calls its Print event.
+        /// <br/><br/>
+        /// Remarks:
+        /// <br/>
+        /// <list type="number">
+        /// <item>Data to print is passed through the global PreReceipt object.</item>
+        /// </list>
+        /// </summary>
+        /// <param name="prereceipt"></param>
+        /// <param name="item"></param>
         public void printTallyLabel(PreReceipts prereceipt,int item)
         {
             if (prereceipt != null)
@@ -108,7 +135,13 @@ namespace NFLInfoCenter.Classes
                
             }
         }
-         
+        /// <summary>
+        /// Reading the value of property DocumentName from PrintingDocument object 
+        /// decides what label construct.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns>True when completing inner taks.</returns>
         public bool print(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
             Console.WriteLine("20 Staring print method inside Printer class");
@@ -123,11 +156,14 @@ namespace NFLInfoCenter.Classes
                 Console.WriteLine("21.2 unit label detected.");
                 printUnitLabelDocument(sender, e);
             }
-
-
             return true;
         }
-
+        /// <summary>
+        /// Tally (PreReceipt) label construction (template)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <param name="item"></param>
         public void printTallyLabelDocument(object sender,System.Drawing.Printing.PrintPageEventArgs e,int item)
         {
             //Setting barcode creator
@@ -198,6 +234,11 @@ namespace NFLInfoCenter.Classes
             //e.Graphics.DrawImage(sfbarcode.ToImage(sfbarcode.Size), new System.Drawing.Point(x - 20, y));
 
         }
+        /// <summary>
+        /// Unit (Receipt) label construction (template)
+        /// </summary>
+        /// <param name="sentder"></param>
+        /// <param name="e"></param>
         public void printUnitLabelDocument(object sentder, System.Drawing.Printing.PrintPageEventArgs e)
         {
             Console.WriteLine("22 Starting Label design");
